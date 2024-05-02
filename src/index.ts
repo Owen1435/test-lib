a69fb4862a4301d473087a5c4cddf289ceff0abf
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-05-02 22:41:48 +0300

    5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * from 'nestjs-otel';
-
 export * from './types';
 export * from './enums';
 export * from './interceptors';
 export * from './metric-producers';
 export * from './otel-sdk';
 export * from './telemetry.module';
+
+export * from 'nestjs-otel';
