270ecea9f2af57aa4065182d874b215b887b26dd
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-05-06 09:58:39 +0300

    32

--- src/interceptors/rmq-trace.interceptor-factory.ts
+++ src/interceptors/rmq-trace.interceptor-factory.ts
@@ -1,6 +1,6 @@
 import { Message, RMQIntercepterClass } from 'nestjs-rmq';
 import { TraceService } from 'nestjs-otel';
-import { SpanStatusCode } from '@opentelemetry/api';
+import { SpanStatusCode, trace } from '@opentelemetry/api';
 
 /** RMQTraceIntercepter factory for proper trace error handling via amqp */
 export function RMQTraceIntercepterFactory(traceService: TraceService): any {
@@ -17,4 +17,29 @@ export function RMQTraceIntercepterFactory(traceService: TraceService): any {
             return res;
         }
     };
+}
+
+export class LogErrorInterceptor extends RMQIntercepterClass {
+    public intercept(res: any, msg: Message, error?: Error): Promise<any> {
+        if (error) {
+            this.logger.error(`Request to topic "${msg.fields.routingKey}" failed.\n${error.stack}`);
+            error.message = `Topic ${msg.fields.routingKey} error: ${error.message}`;
+            throw error;
+        }
+        return res;
+    }
+}
+
+export class RMQTraceIntercepter extends RMQIntercepterClass {
+    public intercept(res: any, msg: Message, error?: Error): Promise<any> {
+        if (error) {
+            const span = trace.getActiveSpan()
+            span?.setStatus({ code: SpanStatusCode.ERROR, message: error.message });
+            span?.addEvent('exception', {
+                ['exception.message']: error.message,
+                ['exception.stacktrace']: error.stack,
+            });
+        }
+        return res;
+    }
 }
