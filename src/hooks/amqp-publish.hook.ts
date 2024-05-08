28f3ee07bace9c5ba98fba9ef28a3518cfa27d7f
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-05-08 14:01:27 +0300

    43

--- src/hooks/amqp-publish.hook.ts
+++ src/hooks/amqp-publish.hook.ts
@@ -3,11 +3,12 @@ import { AmqplibPublishCustomAttributeFunction, PublishInfo } from '@opentelemet
 
 export const amqpPublishHook: AmqplibPublishCustomAttributeFunction = (span: Span, info: PublishInfo) => {
     console.log('amqpPublishHook_', info, trace.getActiveSpan());
-    console.log(span);
+    console.log('span b', span);
     if (info.routingKey.includes('amq.rabbitmq.reply-to')) {
         span.updateName(`AMQP publish reply message`);
         console.log((trace.getActiveSpan() as any)?.resource?.attributes?.['service.name']);
-        span.setAttribute('amq.app.name', (trace.getActiveSpan() as any)?.resource?.attributes?.['service.name']);
+        span.setAttribute('app.name', (trace.getActiveSpan() as any)?.resource?.attributes?.['service.name']);
+        console.log('span a', span)
         return;
     }
 
