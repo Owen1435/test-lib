574bd8917f2fc688d1407bf614a4929a0b5b8484
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-05-08 13:16:10 +0300

    37

--- src/hooks/amqp-consume.hook.ts
+++ src/hooks/amqp-consume.hook.ts
@@ -1,7 +1,12 @@
-import { Span } from '@opentelemetry/api';
+import { Span, trace } from '@opentelemetry/api';
 import { AmqplibConsumeCustomAttributeFunction, ConsumeInfo } from '@opentelemetry/instrumentation-amqplib';
 
 export const amqpConsumeHook: AmqplibConsumeCustomAttributeFunction = (span: Span, info: ConsumeInfo) => {
-    console.log('amqpConsumeHook', info);
+    console.log('amqpConsumeHook_', info, trace.getActiveSpan()?.spanContext());
+    if (info.msg.fields.routingKey.includes('amq.rabbitmq.reply-to')) {
+        span.updateName(`AMQP consume reply message`);
+        return;
+    }
+
     span.updateName(`AMQP consume ${info.msg.fields.exchange}->${info.msg.fields.routingKey}`);
 };
--- src/hooks/amqp-publish.hook.ts
+++ src/hooks/amqp-publish.hook.ts
@@ -1,7 +1,12 @@
-import { Span } from '@opentelemetry/api';
+import { Span, trace } from '@opentelemetry/api';
 import { AmqplibPublishCustomAttributeFunction, PublishInfo } from '@opentelemetry/instrumentation-amqplib';
 
 export const amqpPublishHook: AmqplibPublishCustomAttributeFunction = (span: Span, info: PublishInfo) => {
-    console.log('amqpPublishHook', info);
+    console.log('amqpPublishHook_', info, trace.getActiveSpan()?.spanContext());
+    if (info.routingKey.includes('amq.rabbitmq.reply-to')) {
+        span.updateName(`AMQP publish reply message`);
+        return;
+    }
+
     span.updateName(`AMQP publish ${info.exchange}->${info.routingKey}`);
 };
