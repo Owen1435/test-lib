7488af69dc3b480e8ab7d26ab1e97ee911d1aed7
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-05-08 12:52:45 +0300

    36

--- src/hooks/amqp-consume.hook.ts
+++ src/hooks/amqp-consume.hook.ts
@@ -1,6 +1,7 @@
 import { Span } from '@opentelemetry/api';
-import { AmqplibConsumeCustomAttributeFunction, ConsumeInfo } from '@opentelemetry/instrumentation-amqplib/build/src/types';
+import { AmqplibConsumeCustomAttributeFunction, ConsumeInfo } from '@opentelemetry/instrumentation-amqplib';
 
 export const amqpConsumeHook: AmqplibConsumeCustomAttributeFunction = (span: Span, info: ConsumeInfo) => {
+    console.log('amqpConsumeHook', info);
     span.updateName(`AMQP consume ${info.msg.fields.exchange}->${info.msg.fields.routingKey}`);
 };
--- src/hooks/amqp-publish.hook.ts
+++ src/hooks/amqp-publish.hook.ts
@@ -1,6 +1,7 @@
 import { Span } from '@opentelemetry/api';
-import { AmqplibPublishCustomAttributeFunction, PublishInfo } from '@opentelemetry/instrumentation-amqplib/build/src/types';
+import { AmqplibPublishCustomAttributeFunction, PublishInfo } from '@opentelemetry/instrumentation-amqplib';
 
 export const amqpPublishHook: AmqplibPublishCustomAttributeFunction = (span: Span, info: PublishInfo) => {
+    console.log('amqpPublishHook', info);
     span.updateName(`AMQP publish ${info.exchange}->${info.routingKey}`);
 };
