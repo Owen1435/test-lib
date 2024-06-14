2d7e4ab0f89b329f1bc13cfab028a22c8ddad165
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-06-14 10:34:32 +0300

    52

--- src/hooks/kafka-publish.hook.ts
+++ src/hooks/kafka-publish.hook.ts
@@ -1,10 +1,10 @@
 import { Span } from '@opentelemetry/api';
 import { KafkaProducerCustomAttributeFunction } from 'opentelemetry-instrumentation-kafkajs';
-import { kafkaTopicProduceCountMetric } from '../metrics';
+// import { kafkaTopicProduceCountMetric } from '../metrics';
 
 export const kafkaPublishHook: KafkaProducerCustomAttributeFunction = (span: Span, topic: string) => {
     console.log('kafkaPublishHook', topic)
-    console.log(kafkaTopicProduceCountMetric)
-    kafkaTopicProduceCountMetric.add(1, { topic });
+    // console.log(kafkaTopicProduceCountMetric)
+    // kafkaTopicProduceCountMetric.add(1, { topic });
     span.updateName(`Kafka publish ${topic}`);
 };
--- src/metrics.ts
+++ src/metrics.ts
@@ -3,22 +3,22 @@ import { meter } from "./otel-sdk";
 
 export const APPLICATION_METRIC_PREFIX = 'app';
 
-export type TKafkaTopicProduceCountMetricAttributes = {
-    topic: string;
-};
-
-/** The number of produce message count to kafka */
-export const kafkaTopicProduceCountMetric = meter.createCounter<TKafkaTopicProduceCountMetricAttributes>('kafka_topic_produce_count', {
-    description: 'The number of produce message count to kafka',
-    valueType: ValueType.INT,
-});
-
-export type TKafkaTopicConsumeCountMetricAttributes = {
-    topic: string;
-};
-
-/** The number of consume message count to kafka */
-export const kafkaTopicConsumeCountMetric = meter.createCounter<TKafkaTopicConsumeCountMetricAttributes>('kafka_topic_consume_count', {
-    description: 'The number of consume message count to kafka',
-    valueType: ValueType.INT,
-});
+// export type TKafkaTopicProduceCountMetricAttributes = {
+//     topic: string;
+// };
+//
+// /** The number of produce message count to kafka */
+// export const kafkaTopicProduceCountMetric = meter.createCounter<TKafkaTopicProduceCountMetricAttributes>('kafka_topic_produce_count', {
+//     description: 'The number of produce message count to kafka',
+//     valueType: ValueType.INT,
+// });
+//
+// export type TKafkaTopicConsumeCountMetricAttributes = {
+//     topic: string;
+// };
+//
+// /** The number of consume message count to kafka */
+// export const kafkaTopicConsumeCountMetric = meter.createCounter<TKafkaTopicConsumeCountMetricAttributes>('kafka_topic_consume_count', {
+//     description: 'The number of consume message count to kafka',
+//     valueType: ValueType.INT,
+// });
