51a27d8cc625f980fddc0177e756ccaa267d159f
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2024-06-14 10:20:03 +0300

    46

--- src/hooks/kafka-consume.hook.ts
+++ src/hooks/kafka-consume.hook.ts
@@ -1,9 +1,10 @@
 import { Span } from '@opentelemetry/api';
 import { KafkaConsumerCustomAttributeFunction } from 'opentelemetry-instrumentation-kafkajs';
-import { kafkaTopicConsumeCountMetric } from '../metrics';
+import {kafkaTopicConsumeCountMetric, kafkaTopicProduceCountMetric} from '../metrics';
 
 export const kafkaConsumeHook: KafkaConsumerCustomAttributeFunction = (span: Span, topic: string) => {
     console.log('kafkaConsumeHook', topic)
+    console.log(kafkaTopicConsumeCountMetric)
     kafkaTopicConsumeCountMetric.add(1, { topic });
     span.updateName(`Kafka consume ${topic}`);
 };
--- src/hooks/kafka-publish.hook.ts
+++ src/hooks/kafka-publish.hook.ts
@@ -4,6 +4,7 @@ import { kafkaTopicProduceCountMetric } from '../metrics';
 
 export const kafkaPublishHook: KafkaProducerCustomAttributeFunction = (span: Span, topic: string) => {
     console.log('kafkaPublishHook', topic)
+    console.log(kafkaTopicProduceCountMetric)
     kafkaTopicProduceCountMetric.add(1, { topic });
     span.updateName(`Kafka publish ${topic}`);
 };
